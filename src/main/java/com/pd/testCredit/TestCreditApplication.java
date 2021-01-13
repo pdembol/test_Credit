4b33027796315f90c0fef93f1adbaa14c10982e2
pdembol <debicki.piotr90@gmail.com>
2021-01-13 17:00:54 +0100

    first commit

---
+++ src/main/java/com/pd/testCredit/TestCreditApplication.java
@@ -0,0 +1,13 @@
+package com.pd.testCredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pd/testCredit/TestCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.testCredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
